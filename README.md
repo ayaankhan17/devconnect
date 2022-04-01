3dfac329b17022652f1ae6eed117361a972965ba
Ayaan Khan <ak.ayaan39@gmail.com>
2022-04-01 21:12:13 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+﻿# Devconnector
+A Social Network For Developers to connect with other Developers
+A MERN Stack application 
+Created With help of ReactJS at the Frontend and NodeJS for backend along with MongoDB for Database
+The Application has been hosted with the help of HEROKU.
+It is Live here https://pure-chamber-24121.herokuapp.com/
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const connectDB = require('./config/db')
+const path = require('path');
+
+const app = express();
+//connect DB
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false })); 
+
+//app.get('/',(req,res) => res.send('API running'));
+
+// Define Routes
+app.use('/api/users',require('./Route/Api/users'));
+app.use('/api/auth',require('./Route/Api/auth'));
+app.use('/api/posts',require('./Route/Api/posts'));
+app.use('/api/profile',require('./Route/Api/profile'));
+
+//Serve static assets in production 
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+    })
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,() => console.log('Server Started in port ${PORT}'))
+//test
